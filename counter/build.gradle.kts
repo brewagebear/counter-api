eea642ad6e6a59b62cba39cf90d2b4a6e39c7675
brewagebear <workingssu@gmail.com>
2023-07-04 21:53:02 +0900

    chore : gralde 초기 설정

---
+++ counter/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.1"
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+group = "io.github.brewagebear"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
