006cb8fe27c9c9561ab7e7606a67c898ebeb329d
brewagebear <workingssu@gmail.com>
2023-07-04 21:54:10 +0900

    feat : Validator 추가  - UrlValidator : URL 검증 클래스  - IPv4Validator : IPv4 검증 클래스

---
+++ counter/src/main/java/io/github/brewagebear/usecase/exceptions/ValidateException.java
@@ -0,0 +1,7 @@
+package io.github.brewagebear.usecase.exceptions;
+
+public class ValidateException extends RuntimeException{
+    public ValidateException(String message) {
+        super(message);
+    }
+}
